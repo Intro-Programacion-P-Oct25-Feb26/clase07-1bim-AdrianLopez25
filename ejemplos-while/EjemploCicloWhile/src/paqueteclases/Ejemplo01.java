73a1afd6c4668e2bbf04c4ae5dcdd5aa660399a2
Adrian Lopez <israelruiz2874@gmail.com>
2025-11-19 11:56:43 -0500

    Subir al repositorio

---
+++ ejemplos-while/EjemploCicloWhile/src/paqueteclases/Ejemplo01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteclases;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Ejemplo01 {
+public static void main(String[] args) {
+    int contador = 1;
+    int limite = 10;
+    
+    while(contador <=limite){
+        System.out.printf("Contador %d\n", contador);
+        contador = contador +1;
+    }
+}
+}
